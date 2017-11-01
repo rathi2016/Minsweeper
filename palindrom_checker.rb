edfc0307fe3625b18c15a9c00238fcd9dafa1618
Rathi <github email address>
2017-11-01 13:14:26 -0400

    practising code challenge

---
+++ palindrom_checker.rb
@@ -0,0 +1,9 @@
+# Check if characters of a given string can be rearranged to form a palindrome:
+
+# we can have two separate function
+# 1- checking if the given string satifies conditions for it do be a palindrome
+  #   at most one character occurs odd number of times and all characters occur even number of times.
+# 2- to convert given string into palidrome
+class PalindromConverter
+  
+end
